20ea32db5411263bfd27a72ff4e8cb7475d9c60d
hoon2 <hoon2405@naver.com>
2022-12-25 16:52:34 +0900

    홈, 로그인, 회원가입 페이지 추가

---
+++ src/routes/home/Home.js
@@ -0,0 +1,7 @@
+function Home() {
+    return (
+        <div>홈페이지</div>
+    )
+}
+
+export default Home;
---
+++ src/routes/join/Join.js
@@ -0,0 +1,7 @@
+function Join() {
+    return (
+        <div>회원가입 페이지</div>
+    )
+}
+
+export default Join;

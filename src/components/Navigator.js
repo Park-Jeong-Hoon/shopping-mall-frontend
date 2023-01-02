30785b981f507de1020b23fac3849216c4b76f93
hoon2 <hoon2405@naver.com>
2023-01-02 19:56:53 +0900

    부트스트랩 적용, 상품목록 페이지 백엔드와 통신

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,12 +1,34 @@
-import { Link } from "react-router-dom";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function Navigator() {
+
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <div>네비게이션</div>
-            <Link to={"/items"}>상품목록</Link>
-            <Link to={"/orders"}>주문목록</Link>
-        </div>
+        <Navbar bg="light" varient="light" >
+            <Container>
+                <Navbar.Brand href="/">홈</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto" onSelect={(selectedKey) => navigate(`/${selectedKey}`)}>
+                        <Nav.Link eventKey="items">상품목록</Nav.Link>
+                        <Nav.Link eventKey="orders">주문목록</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
--- src/routes/item/ItemList.js
+++ src/routes/item/ItemList.js
@@ -1,6 +1,63 @@
-function ItemList() {
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function ItemList({ isLogin, setLogin }) {
+
+    const [items, setItems] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+
+    const getItems = async () => {
+        await axios(
+            {
+                url: '/item/all',
+                method: 'get',
+                baseURL: `${process.env.REACT_APP_BACKEND}`,
+                withCredentials: true,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        ).then(function (response) {
+            let jwtHeader = response.headers.get("Authorization")
+            let jwtToken = '';
+            console.log(response);
+            if (jwtHeader !== undefined) {
+                if (jwtHeader.startsWith('Bearer ')) {
+                    jwtToken = jwtHeader.replace('Bearer ', '');
+                }
+                console.log(jwtToken)
+                axios.defaults.headers.common[
+                    "Authorization"
+                ] = `Bearer ${jwtToken}`;
+            }
+            setLoading(false);
+            setItems(response.data);
+        }).catch(error => console.error('Error:', error));
+    }
+
+    useEffect(() => {
+        getItems();
+    }, [])
+
     return (
-        <div>아이템목록</div>
+        <div>
+            <h1>상품목록</h1>
+            <hr />
+            {
+                isLoading === false ?
+                    <div>
+                        {items.map(function (a) {
+                            return (
+                                <div className="list">
+                                    <h3>{a.name}</h3>
+                                    <p>hello</p>
+                                    <hr />
+                                </div>
+                            );
+                        })}
+                    </div> : null
+            }
+        </div>
     )
 }
 
